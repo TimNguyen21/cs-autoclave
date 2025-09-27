de1b8fb62dad64e1952917fb027157eaa404567b
TimNguyen21 <tim.nguyen2112@gmail.com>
2025-09-27 00:33:49 -0600

    [main] Added error handling for empty or invalid input fields when confim completion of loads in Active Loads page

--- src/components/load-completion-confirmation/LoadCompletionConfirmation.jsx
+++ src/components/load-completion-confirmation/LoadCompletionConfirmation.jsx
@@ -10,9 +10,40 @@ function LoadCompletionConfirmation({ loadId, cancelLoadConfirmation }) {
     const dispatch = useDispatch();
 
     const [showConfirmationPopup, setShowConfirmationPopup] = useState(false);
+    const [showFormErrorPopup, setShowFormErrorPopup] = useState(false);
     const [passStatus, setPassStatus] = useState('');
     const [note, setNote] = useState('');
     const [technicianSignoffId, setTechnicianSignoffId] = useState('');
+    const [passStatusError, setPassStatusError] = useState(false);
+    const [noteError, setNoteError] = useState(false);
+    const [technicianSignoffIdError, setTechnicianSignoffIdError] = useState(false);
+
+    const validateForm = () => {
+        let formValid = true;
+
+        if (passStatus === '') {
+            setPassStatusError(true);
+            formValid = false;
+        } else {
+            setPassStatusError(false);
+        }
+
+        if ((passStatus === 'no' || passStatus === 'na') && note.trim() === '') {
+            setNoteError(true);
+            formValid = false;
+        } else {
+            setNoteError(false);
+        }
+
+        if (technicianSignoffId.trim() === '') {
+            setTechnicianSignoffIdError(true);
+            formValid = false;
+        } else {
+            setTechnicianSignoffIdError(false);
+        }
+
+        return formValid;
+    }
 
     const clearConfirmationForm = () => {
         setPassStatus('');
@@ -24,7 +55,9 @@ function LoadCompletionConfirmation({ loadId, cancelLoadConfirmation }) {
         <div className="load-completion-confirmation">
             <div className="load-completion-confirmation__input">
                 <label>Passed?:</label>
-                <select defaultValue="" onChange={(e) => setPassStatus(e.target.value)}>
+                <select className={passStatusError ? 'load-completion-confirmation__input-has-error' : ''}
+                        defaultValue=""
+                        onChange={(e) => setPassStatus(e.target.value)}>
                     <option value=""></option>
                     <option value="yes">Yes</option>
                     <option value="no">No</option>
@@ -33,20 +66,28 @@ function LoadCompletionConfirmation({ loadId, cancelLoadConfirmation }) {
             </div>
             <div className='load-completion-confirmation__input-notes'>
                 <label>
-                    Notes: <span>*Note Optional if Passed / <span className='load-completion-confirmation__red-text'>REQUIRED NOTE if Failed or N/A selected*</span></span>
+                    Notes: <span>*Note Optional if Passed / <span className={noteError ? 'load-completion-confirmation__red-text' : ''}>REQUIRED NOTE if Failed or N/A selected</span>*</span>
                 </label>
-                <textarea placeholder="Enter notes here..." onChange={(e) => setNote(e.target.value)}/>
+                <textarea className={noteError ? 'load-completion-confirmation__input-has-error' : ''}
+                          placeholder="Enter notes here..."
+                          onChange={(e) => setNote(e.target.value)}/>
             </div>
             <div className='load-completion-confirmation__input-tech-id'>
                 <label>Technician ID:</label>
-                <input type="number" min='0' onChange={(e) => setTechnicianSignoffId(e.target.value)}/>
+                <input className={technicianSignoffIdError ? 'load-completion-confirmation__input-has-error' : ''}
+                       type="number"
+                       min='0'
+                       onChange={(e) => setTechnicianSignoffId(e.target.value)}/>
             </div>
             <div className='load-completion-confirmation__button-actions'>
                 <Button label="Cancel" variant="cancel" onClick={cancelLoadConfirmation} />
-                <Button label="Confirm Load Completion" onClick={() => setShowConfirmationPopup(true)} />
+                <Button label="Confirm Load Completion"
+                        onClick={() => {
+                            validateForm() ? setShowConfirmationPopup(true) : setShowFormErrorPopup(true);
+                        }} />
             </div>
             <PopupConfirmation
-            message={"Are you sure you want to complete this load?"}
+                message={"Are you sure you want to complete this load?"}
                 confirmButtonLabel="Yes, Complete Load"
                 onConfirm={() => {
                     dispatch(confirmLoadCompletion({
@@ -62,6 +103,13 @@ function LoadCompletionConfirmation({ loadId, cancelLoadConfirmation }) {
                 cancelButtonLabel="No, Go Back"
                 onCancel={() => setShowConfirmationPopup(false)}
                 open={showConfirmationPopup}/>
+            <PopupConfirmation
+                message={"There is an error with the form. Please check the fields and try again."}
+                confirmButtonLabel="Ok"
+                onConfirm={() => {
+                    setShowFormErrorPopup(false);
+                }}
+                open={showFormErrorPopup}/>
         </div>
     );
 }   
--- src/components/load-summary/LoadSummary.jsx
+++ src/components/load-summary/LoadSummary.jsx
@@ -2,15 +2,16 @@ import './LoadSummary.scss';
 import { formatDateMMDDYYYYWithSlash } from '../../utils/dateUtils';
 
 function LoadSummary({ loadData }) {
+    const { loadId, date, technicianId, autoclaveNumber, loadNumber, items } = loadData;
 
     return (
         <div className="load-summary">
-            <div><b>Load ID:</b> {loadData.loadId}</div>
-            <div><b>Date:</b> {formatDateMMDDYYYYWithSlash(loadData.date)}</div>
-            <div><b>Technician ID:</b> {loadData.technicianId}</div>
-            <div><b>Autoclave:</b> {loadData.autoclaveNumber}</div>
-            <div><b>Load:</b> {loadData.loadNumber}</div>
-            <div><b>Items Summary:</b> {loadData.items}</div>
+            <div><b>Load ID:</b> {loadId}</div>
+            <div><b>Date:</b> {formatDateMMDDYYYYWithSlash(date)}</div>
+            <div><b>Technician ID:</b> {technicianId}</div>
+            <div><b>Autoclave:</b> {autoclaveNumber}</div>
+            <div><b>Load:</b> {loadNumber}</div>
+            <div><b>Items Summary:</b> {items}</div>
         </div>
     );
 }
