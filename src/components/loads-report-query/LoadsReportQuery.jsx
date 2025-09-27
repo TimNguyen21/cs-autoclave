2139810eaeda3667599f3c545061e250f014ff08
TimNguyen21 <tim.nguyen2112@gmail.com>
2025-09-27 04:16:24 -0600

    [main] initial setup of Loads Report Query component for Loads Report page

---
+++ src/components/loads-report-query/LoadsReportQuery.jsx
@@ -0,0 +1,12 @@
+import './LoadsReportQuery.scss';
+
+function LoadsReportQuery() {
+
+    return (
+        <form className='loads-report-query'>
+            load report Query
+        </form>
+    )
+}
+
+export default LoadsReportQuery;
--- src/containers/loads-report/LoadsReport.jsx
+++ src/containers/loads-report/LoadsReport.jsx
@@ -1,10 +1,15 @@
+import LoadsReportQuery from '../../components/loads-report-query/LoadsReportQuery';
 import './LoadsReport.scss';
 
 function LoadsReport() {
 
     return (
         <main className='loads-report'>
-            loads report
+            <div className='loads-report__summary'>
+                <div className='loads-report__summary-query'>
+                    <LoadsReportQuery />
+                </div>
+            </div>
         </main>
     )
 }
