95dd3c37d4c6f5a6379d4facfc1075978d0c96c3
TimNguyen21 <tim.nguyen2112@gmail.com>
2025-09-29 02:50:36 -0600

    [main] moved allocource icon from public to root directory

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
 
     return (
         <header className='header'>
-            <img className='header__logo' src='/public/allosource-logo.png' alt='AlloSource Logo' />
+            <img className='header__logo' src='/allosource-logo.png' alt='AlloSource Logo' />
         </header>
     )
 }
