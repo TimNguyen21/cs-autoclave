59cc10525c17d2858338093b7b1cb215cf39f6f0
TimNguyen21 <tim.nguyen2112@gmail.com>
2025-09-29 02:02:12 -0600

    [main] loadData is saved in localStorage where data stays consistent on page reload

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,8 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
 import autoclaveLoadsReducer from '../features/autoclave-loads/autoclaveLoadsSlice';
 
+// Function to load state from localStorage
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('reduxState');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+// Function to save state to localStorage
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('reduxState', serializedState);
+  } catch (err) {
+    alert('Error saving state to localStorage');
+  }
+};
+
+const preloadedState = loadState();
+
 export const store = configureStore({
   reducer: {
     autoclaveLoads: autoclaveLoadsReducer,
   },
+  preloadedState,
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
