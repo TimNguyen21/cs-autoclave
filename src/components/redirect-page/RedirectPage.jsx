e32429f2e4a3e70e1538868cee627e0cbe76bb29
TimNguyen21 <tim.nguyen2112@gmail.com>
2025-10-05 04:54:08 -0600

    [main] updated redirect page to Home if user enters an invalid URL link

--- src/components/redirect-page/RedirectPage.jsx
+++ src/components/redirect-page/RedirectPage.jsx
@@ -3,13 +3,17 @@ import Button from '../button/Button';
 
 function RedirectPage() {
 
+    setTimeout(function() {
+        window.location.href = "/cs/autoclave/";
+    }, 7000);
+
     return (
         <main className='redirect-page'>
             <div className='redirect-page__container'>
                 <h2>Page Not Found</h2>
                 <p>The page you are looking for does not exist.</p>
-                <p>Please click the button below. You will be redirected to the Load Entry page.</p>
-                <Button label="Load Entry" onClick={() => window.location.href="/cs/autoclave/load-entry"} />
+                <p>You will be automatically redirected to the Home page. If you are not redirected, please click the button below.</p>
+                <Button label="Home" onClick={() => window.location.href="/cs/autoclave/"} />
             </div>
         </main>
     )
